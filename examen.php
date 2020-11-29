24ad216325c54d933678a0a8db4e459d6f3607f0
RafenixOG <r-nuhic@hotmail.com>
2020-11-29 16:37:06 +0100

    Ajout de l'option "Je ne sais pas" dans le calcul de la note

--- examen.php
+++ examen.php
@@ -15,7 +15,7 @@ $resultEnCours = mysqli_query($conn, $sqlEnCours);
 echo "<html><head><title>Examen de ". $_POST["bouton"]. "</title></head><body>";
 
 echo "<p><h1>Examen de ". $_POST["bouton"]. "</h1></p><p>Répondez aux questions ci-dessous.<br>Votre note sera calculé suivant ";
-echo "cette logique :<ul><li>Bonne réponse : +1</li><li>Mauvaise réponse : -0,5</li><li>Aucune réponse : 0</li></ul>Une fois ";
+echo "cette logique :<ul><li>Bonne réponse : +1</li><li>Mauvaise réponse : -0,5</li><li>Aucune réponse ou \"Je ne sais pas\" : 0</li></ul>Une fois ";
 echo "terminer, <b>valider vos réponses à l'aide du bouton <i>VALIDER</i></b></p>";
 echo "<p><b>ATTENTION !!!</b> Veillez à ne pas fermer votre navigateur ou quitter cette page avant de terminer et d'appuyer sur le bouton de validation.";
 echo "<br>Le non respect de cette consigne entrainera un <b>ZÉRO immédiat à l'examen !</b></p>";
@@ -37,8 +37,8 @@ while($row = mysqli_fetch_assoc($resultQuestion)) {
 		echo "<input type='radio' id='". $repRow["id"]. "' name='". $i. "' value='". $repRow["id"]. "'>";
 		echo "<label for='". $repRow["id"]. "'>". $repRow["intitule"]. "</label><br>";
 	}
-	echo "<input type='radio' id='jsp' name='". $i. "' value='jsp'>";
-        echo "<label for='jsp'>Je ne sais pas</label><br><br>";
+	echo "<input type='radio' id='jsp". $i. "' name='". $i. "' value='jsp'>";
+        echo "<label for='jsp". $i. "'>Je ne sais pas</label><br><br>";
 	$i++;
 }
 ?>
